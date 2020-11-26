11fe27b377beca4b7291f4939346302cc95a6be9
Roman Pylenkov <solodov1994roma@gmail.com>
2020-11-26 03:58:35 +0300

    Added tests for HomeController Index method (red phase)

---
+++ WebAppTDD.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+using NUnit.Framework;
+
+using WebAppTDD.Controllers;
+
+namespace WebAppTDD.Tests.Controllers
+{
+    [TestFixture]
+    public class HomeControllerTest
+    {
+        [Test]
+        public void IndexNotNullTest()
+        {
+            HomeController homeController = new HomeController();
+
+            ViewResult result = homeController.Index() as ViewResult;
+
+            Assert.NotNull(result);
+        }
+
+        [Test]
+        public void IndexReturnsIndexCshtml()
+        {
+            HomeController homeController = new HomeController();
+
+            ViewResult result = homeController.Index() as ViewResult;
+
+            Assert.AreEqual("Index", result.ViewName);
+        }
+
+        [Test]
+        public void IndexReturnsScheduleModel()
+        {
+            HomeController homeController = new HomeController();
+
+            ViewResult result = homeController.Index() as ViewResult;
+
+            Assert.IsInstanceOf<IEnumerable<Class>>(result.Model);
+        }
+    }
+}
