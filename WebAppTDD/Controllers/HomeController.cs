568d27db6f98f118bc19b263a42d4b947bcaa733
Roman Pylenkov <solodov1994roma@gmail.com>
2020-11-26 04:15:25 +0300

    Rewrited HomeController according to IRepository interface

--- WebAppTDD/Controllers/HomeController.cs
+++ WebAppTDD/Controllers/HomeController.cs
@@ -4,10 +4,20 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using WebAppTDD.Data;
+using WebAppTDD.Models;
+
 namespace WebAppTDD.Controllers
 {
     public class HomeController : Controller
     {
+        private IRepository<Class> classesRepository;
+
+        public HomeController(IRepository<Class> repository)
+        {
+            classesRepository = repository;
+        }
+
         public ActionResult Index()
         {
             return View();
