135cba339ce9c5f9bd8201a19227ce0fcb3fbf1d
Roman Pylenkov <solodov1994roma@gmail.com>
2020-11-26 04:01:44 +0300

    Added model class

---
+++ WebAppTDD/Models/Class.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppTDD.Models
+{
+    public class Class
+    {
+        public DateTime Date { get; set; }  
+        public String Location { get; set; }
+
+        public string Discipline { get; set; }  
+        public string Teacher { get; set; }
+    }
+}
